a24977c137ecc000f689f578eb12f21f68ad7b02
ani <godwin@istrategytech.com>
2023-05-25 22:13:12 +0100

    maps and sets

--- app/src/main/java/com/example/aniskotlinjourney/maps/Maps.kt
+++ app/src/main/java/com/example/aniskotlinjourney/maps/Maps.kt
@@ -2,4 +2,15 @@ package com.example.aniskotlinjourney.maps
 
 fun main(){
 
+    // immutable map
+
+    var immMap = mapOf("name" to "ani", "job" to "swe")
+    print(immMap["job"])
+
+
+    //mutable map
+
+    var mutableMap = mutableMapOf("name" to "ani","house" to "maitama")
+    print(mutableMap)
+
 }
